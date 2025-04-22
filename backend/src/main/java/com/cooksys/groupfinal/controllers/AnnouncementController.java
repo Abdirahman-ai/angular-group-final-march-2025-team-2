3e46e305ef04309e5b3f63efa88bc2567f95ddb2
Abdirahman-ai <62496069+Abdirahman-ai@users.noreply.github.com>
2025-04-22 12:01:13 -0500

    Finished POST /announcement/company/{companyId} endpoint #27

--- backend/src/main/java/com/cooksys/groupfinal/controllers/AnnouncementController.java
+++ backend/src/main/java/com/cooksys/groupfinal/controllers/AnnouncementController.java
@@ -1,7 +1,7 @@
 package com.cooksys.groupfinal.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.cooksys.groupfinal.dtos.AnnouncementDto;
+import org.springframework.web.bind.annotation.*;
 
 import com.cooksys.groupfinal.services.AnnouncementService;
 
@@ -14,4 +14,8 @@ public class AnnouncementController {
 	
 	private final AnnouncementService announcementService;
 
+	@PostMapping("/company/{companyID}")
+	public AnnouncementDto createAnnouncement(@PathVariable long companyID, @RequestBody AnnouncementDto announcementDto){
+		return announcementService.createAnnouncement(companyID, announcementDto);
+	}
 }
